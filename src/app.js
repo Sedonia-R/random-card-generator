2f7afa29de9f3fef742b96b320aeee82a3cb1c09
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-06-19 02:21:34 +0000

    worked on html, css, and js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,57 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your code here\
+  const cardValue = document.querySelector("strong");
+  cardValue.innerHTML = chooseRandomItem(cardValues);
+
+  const cardSuit = document.querySelector(".suit-icon");
+  cardSuit.innerHTML = chooseRandomItem(cardSuits);
 };
+
+const heartsImageTag =
+  '<img src="../src/assets/img/heart.svg" alt="hearts suit icon" class="img-size"/>';
+const spadesImageTag =
+  '<img src="../src/assets/img/spade.svg" alt="spades suit icon" class="img-size">';
+const clubsImageTag =
+  '<img src="../src/assets/img/club.svg" alt="clubs suit icon" class="img-size">';
+const diamondsImageTag =
+  '<img src="../src/assets/img/diamond.svg" alt="diamonds suit icon" class="img-size">';
+
+const cardSuits = [
+  heartsImageTag,
+  spadesImageTag,
+  clubsImageTag,
+  diamondsImageTag
+];
+
+const cardValues = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
+function chooseRandomItem(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+// const cardValue = document.querySelector("strong");
+// cardValue.innerHTML = chooseRandomItem(cardValues);
+
+// const cardSuit = document.querySelector(".suit-icon");
+// cardSuit.innerHTML = chooseRandomItem(cardSuits);
