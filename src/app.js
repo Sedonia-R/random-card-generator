2518feca77d87f6efa56f626b0c0d67314f92504
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-06-21 02:36:36 +0000

    first commit - built a full card with css and html and began building card using js and dom

--- src/app.js
+++ src/app.js
@@ -1,33 +1,37 @@
-/* eslint-disable */
+// /* eslint-disable */
 import "bootstrap";
 import "./style.css";
 // import { NoEmitOnErrorsPlugin } from "webpack";
 
 window.onload = function() {
   if (document.readyState === "complete") {
-    const cardValue = document.querySelector("strong");
-    cardValue.innerHTML = chooseRandomItem(cardValues);
+    // const cardValue = document.querySelector("strong");
+    // cardValue.innerHTML = chooseRandomItem(cardValues);
 
-    const cardSuits = document.querySelectorAll(".suit-icon");
-    const suit = chooseRandomItem(suitTags);
-    for (let cardSuit of cardSuits) {
-      cardSuit.innerHTML = suit;
-    }
+    // const cardSuits = document.querySelectorAll(".suit-icon");
+    // const suit = chooseRandomItem(suitTags);
+    // for (let cardSuit of cardSuits) {
+    //   cardSuit.innerHTML = suit;
+    // }
 
-    const button = document.querySelector("button");
-    button.addEventListener("click", refreshPage);
-    button.style.display = "block";
+    const card = document.querySelector(".card");
+    const newTopRow = buildTopOrBottomRow("top");
+    // const newMiddleRow = buildMiddleRow();
+    const newBottomRow = buildTopOrBottomRow("bottom");
+    card.appendChild(newTopRow);
+    // card.appendChild(newMiddleRow);
+    card.appendChild(newBottomRow);
+
+    // const button = document.querySelector("button");
+    // button.addEventListener("click", refreshPage);
+    // button.style.display = "block";
   }
 };
 
-const heartsImageTag =
-  '<img src="../src/assets/img/heart.svg" alt="hearts suit icon" class="img-size"/>';
-const spadesImageTag =
-  '<img src="../src/assets/img/spade.svg" alt="spades suit icon" class="img-size">';
-const clubsImageTag =
-  '<img src="../src/assets/img/club.svg" alt="clubs suit icon" class="img-size">';
-const diamondsImageTag =
-  '<img src="../src/assets/img/diamond.svg" alt="diamonds suit icon" class="img-size">';
+const heartsImageTag = "../src/assets/img/heart.svg";
+const spadesImageTag = "../src/assets/img/spade.svg";
+const clubsImageTag = "../src/assets/img/club.svg";
+const diamondsImageTag = "../src/assets/img/diamond.svg";
 
 const suitTags = [
   heartsImageTag,
@@ -60,3 +64,42 @@ function chooseRandomItem(array) {
 function refreshPage() {
   window.location.reload();
 }
+
+// const card = document.querySelector(".card");
+
+function buildTopOrBottomRow(sideOfCard) {
+  const topOrBottomRow = document.createElement("div");
+
+  if (sideOfCard === "top") {
+    topOrBottomRow.setAttribute("class", "top-and-bottom");
+  } else if (sideOfCard === "bottom") {
+    topOrBottomRow.setAttribute("class", "top-and-bottom rotated");
+  }
+
+  const p = document.createElement("p");
+  const strong = document.createElement("strong");
+  const image = document.createElement("img");
+  image.setAttribute("class", "corner-img");
+  const cardValue = document.createTextNode(chooseRandomItem(cardValues));
+  strong.appendChild(cardValue);
+  p.appendChild(strong);
+  const suit = chooseRandomItem(suitTags);
+  image.src = suit;
+  topOrBottomRow.appendChild(p);
+  topOrBottomRow.appendChild(image);
+  return topOrBottomRow;
+}
+
+function buildMiddleRow() {
+  const middleRow = document.createElement("div");
+  middleRow.setAttribute("class", "middle");
+  const image = document.createElement("img");
+  image.setAttribute("class", "center-img");
+  const suit = chooseRandomItem(suitTags);
+  image.src = suit;
+  middleRow.appendChild(image);
+  return buildMiddleRow;
+}
+
+// var element = document.getElementById("myDIV");
+// element.classList.add("mystyle");
--- src/notes.js
+++ src/notes.js
@@ -12,3 +12,12 @@
 // bottomCornerSuit.appendChild(suitImgTaG);
 // const secondToBottomDiv = document.querySelector("#second-from-bottom");
 // // secondToBottomDiv.insertAdjacentElement("afterend", bottomCornerSuit);
+
+const heartsImageTag =
+  '<img src="../src/assets/img/heart.svg" alt="hearts suit icon""/>';
+const spadesImageTag =
+  '<img src="../src/assets/img/spade.svg" alt="spades suit icon">';
+const clubsImageTag =
+  '<img src="../src/assets/img/club.svg" alt="clubs suit icon">';
+const diamondsImageTag =
+  '<img src="../src/assets/img/diamond.svg" alt="diamonds suit icon">';
